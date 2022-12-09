df373f55b91bba13a3c86a1538861e5b76c4fa80
Aleksei Sidenko <sidoi90@bk.ru>
2022-12-09 15:50:38 +0300

    Сверстал сайт и подготовил к ревью

---
+++ script.js
@@ -0,0 +1,23 @@
+function redact() {
+  document.querySelector('.popup').style.display = "flex"
+};
+
+function close() {
+  document.querySelector('.popup').style.display = "none"
+};
+
+document.querySelector('.profile__edit-button').addEventListener('click', redact);
+
+document.querySelector('.popup__button-close').addEventListener('click', close);
+
+function editUser() {
+  let name = document.querySelector('.popup__name');
+  let profession = document.querySelector('.popup__profession');
+
+  document.querySelector('.profile__name').textContent = `${name.value}`;
+  document.querySelector('.profile__profession').textContent = `${profession.value}`;
+
+  close();
+};
+
+document.querySelector('.popup__button-save').addEventListener('click', editUser, close);
